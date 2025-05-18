237632aeca1900df1b50914d902fd3c5b5334df0
andreAnalytics <elias.r.andrew@gmail.com>
2025-05-18 16:34:31 +0200

    this is an initial commit to the assignment

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("hello, Space X data engineer")
+
+# Integer
+launch_count = 10
+
+# Float
+success_rate = 0.95
+
+# String
+rocket_name = "Falcon 9"
+
+# Boolean
+is_active = True
+
+# this is just a test
+total_launches = launch_count + 5
+print(total_launches)
---
+++ variables.py
@@ -0,0 +1,22 @@
+# Assignment
+# Create a new Python file called variables.py.
+# Define variables for:
+# Number of launches.
+# Name of the rocket.
+# Launch success rate.
+# Print these variables to the console.
+# Add comments to explain your code.
+# Save your work and commit it to your GitHub repository.
+
+# number of launches
+no_launch = 5
+print(no_launch)
+
+# name of the rocket
+rocket_name = "Marc 4"
+print(rocket_name)
+
+# launch success rate
+success_rate = 0.9
+print(success_rate)
+
