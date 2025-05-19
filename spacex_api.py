6328ab59e60de94833631332a2ed98e680184b9b
andreAnalytics <elias.r.andrew@gmail.com>
2025-05-19 19:49:14 +0200

    Modified API to use Pandas

--- spacex_api.py
+++ spacex_api.py
@@ -1,9 +1,23 @@
-import requests
+#the below lines fetch data using an API
+
+# import requests
+# import pandas as pd
+
+# response = requests.get('https://api.spacexdata.com/v4/launches')
+# data = response.json()
+
+# df = pd.DataFrame(data)
+# print(df.head())
+
+# the below line fetch data directly using Pandas
+
 import pandas as pd
 
-response = requests.get('https://api.spacexdata.com/v4/launches')
-data = response.json()
+url = 'https://api.spacexdata.com/v4/launches'
 
-df = pd.DataFrame(data)
-print(df.head())
+df = pd.read_json(url)
+# Print(df.head())
 
+# print(df.columns)
+
+print(df.dtypes)
