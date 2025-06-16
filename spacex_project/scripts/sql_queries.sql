7e4abb18a90cd4df00ae588253fc89e7890e801c
andreAnalytics <elias.r.andrew@gmail.com>
2025-06-16 17:58:33 +0200

    new sql queries added

--- spacex_project/scripts/sql_queries.sql
+++ spacex_project/scripts/sql_queries.sql
@@ -9,3 +9,19 @@ where LAUNCH_DATE= (SELECT MAX(LAUNCH_DATE) from launchestransform);
 SELECT rocket, COUNT(rocket) AS laucnhes_per_rocket
 FROM launchestransform
 GROUP BY 1;
+
+SELECT
+    rocket,
+    AVG (
+        CASE WHEN success = TRUE THEN 1
+        ELSE 0
+        END
+    ) AS success_rate
+FROM dbt_spacex.launchestransform
+GROUP BY rocket;
+
+SELECT
+    YEAR(launch_date) AS launch_year,
+    COUNT(*) AS launches_per_year
+FROM dbt_spacex.launchestransform
+GROUP BY 1;
